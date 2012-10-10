ff6c1f562d5b9e188fe1df839afcc7669d8183c1
Freddie Witherden <freddie@witherden.org>
2012-10-10 13:20:59 +0100

    Add a simple example showcasing the backend API.

---
+++ examples/README.rst
@@ -0,0 +1,13 @@
+=============
+PyFR Examples
+=============
+
+Running Examples
+----------------
+
+Before running the examples in this folder it is first necessary to
+either install PyFR or add the root PyFR directory (../) to
+``PYTHONPATH``::
+
+  freddie@serenity ~/PyFR$ export PYTHONPATH=.:$PYTHONPATH
+  freddie@serenity ~/PyFR$ python examples/backend/basic.py
---
+++ examples/backend/basic.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"""Simple backend example
+
+Demonstrates key backend concepts
+"""
+
+from pyfr.backends import CudaBackend
+import numpy as np
+
+# Number of rows and columns in our matrices
+NROW, NCOL = 20, 70
+
+# Generate two random NROW by NCOL matrices
+np1 = np.random.randn(NROW, NCOL)
+np2 = np.random.randn(NROW, NCOL)
+
+# Create the backend
+be = CudaBackend()
+
+# Allocate matrices on the backend
+m1 = be.matrix(NROW, NCOL, np1)
+m2 = be.matrix(NROW, NCOL, np2)
+
+# Prepare the `ipadd` kernel which computes m1[i,j] = m1[i,j] + 2.0*m2[i,j]
+addkern = be.kernel('ipadd', m1, 2.0, m2)
+
+# Get a kernel queue from the backend
+q = be.queue()
+
+# Execute the `addkern` kernel twice
+q % addkern
+q % addkern
+
+# Get m1 back as a numpy array
+m1_n = m1.get()
+
+# Take the difference between m1_n and (np1 + 4*np2)
+res = np.sum(m1_n - (np1 + 4*np2))
+
+print 'Residual: {:.2g}'.format(res)
