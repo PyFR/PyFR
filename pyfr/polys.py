3919e6776b0c78345a7ce2cb9836fa946ad9c209
Freddie Witherden <freddie@witherden.org>
2012-10-21 16:30:55 +0100

    Refactor the polynomial functions.

--- pyfr/polys.py
+++ pyfr/polys.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from sympy import Poly, legendre_poly, diff
-from sympy.abc import x
+import sympy as sy
+import numpy as np
 
-from mpmath import polyroots
+try:
+    import mpmath as mp
+except ImportError:
+    import sympy.mpmath as mp
 
 def gauss_legendre_points(n):
     """Returns the Gauss-Legendre quadrature points for order *n*
@@ -11,21 +14,21 @@ def gauss_legendre_points(n):
     These are defined as the roots of P_n where P_n is the n'th
     Legendre polynomial.
     """
-    coeffs = Poly(legendre_poly(n, x)).all_coeffs()
+    coeffs = sy.Poly(sy.legendre_poly(n)).all_coeffs()
 
-    roots = polyroots([float(c) for c in coeffs])
+    roots = mp.polyroots([float(c) for c in coeffs])
     return [float(r) for r in roots]
 
-def lobatto_points(n):
+def gauss_lobatto_points(n):
     """Returns the Lobatto quadrature points for order *n*
 
     These are defined as the roots of P'_(n-1) where P'_(n-1) is the
     first derivative of the n'th - 1 Legendre polynomial plus the
     points -1.0 and +1.0.
     """
-    coeffs = Poly(diff(legendre_poly(n-1, x), x)).all_coeffs()
+    coeffs = sy.Poly(sy.legendre_poly(n-1).diff()).all_coeffs()
 
-    roots = polyroots([float(c) for c in coeffs])
+    roots = mp.polyroots([float(c) for c in coeffs])
     return [-1.0] + [float(r) for r in roots] + [1.0]
 
 def sympeval(poly, symbols, vals):
