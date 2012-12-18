19aeff09a9593ba23da1272cba26e44385a631ad
Freddie Witherden <freddie@witherden.org>
2012-12-18 17:38:17 +0000

    Rework pyfr-sim.

--- pyfr/scripts/sim.py
+++ pyfr/scripts/sim.py
@@ -1,45 +1,62 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-
-from argparse import ArgumentParser
+from argparse import ArgumentParser, FileType
 
 import numpy as np
 from mpmath import mp
 
+from pyfr import __version__ as version
 from pyfr.backends.cuda import CudaBackend
 from pyfr.inifile import Inifile
 from pyfr.integrators import get_integrator
 from pyfr.rank_allocator import get_rank_allocation
 
+def process_run(args):
+    return np.load(args.mesh), None, Inifile.load(args.cfg)
+
+def process_restart(args):
+    mesh = np.load(args.mesh)
+    soln = np.load(args.soln)
+
+    if args.cfg:
+        cfg = Inifile.load(args.cfg)
+    else:
+        cfg = Inifile(soln['config'].item())
+
+    return mesh, soln, cfg
+
 @mp.workdps(60)
 def main():
-    # Directory configuration
-    cfg_dir, cfg_fname = os.path.split(sys.argv[1])
-    os.chdir(cfg_dir)
+    ap = ArgumentParser(prog='pyfr-sim', description='Runs a PyFR simulation')
+    ap.add_argument('--verbose', '-v', action='count')
 
-    # Load the config file
-    cfg = Inifile.load(cfg_fname)
+    sp = ap.add_subparsers(help='sub-command help')
+
+    ap_run = sp.add_parser('run', help='run --help')
+    ap_run.add_argument('mesh', type=FileType('rb'), help='mesh file')
+    ap_run.add_argument('cfg', type=FileType('r'), help='config file')
+    ap_run.set_defaults(process=process_run)
+
+    ap_restart = sp.add_parser('restart', help='restart --help')
+    ap_restart.add_argument('mesh', type=FileType('rb'), help='mesh file')
+    ap_restart.add_argument('soln', type=FileType('rb'), help='solution file')
+    ap_restart.add_argument('cfg', nargs='?', type=FileType('r'),
+                            help='new config file')
+    ap_restart.set_defaults(process=process_restart)
+
+    # Parse the arguments
+    args = ap.parse_args()
+    mesh, soln, cfg = args.process(args)
 
     # Create a backend
     backend = CudaBackend()
 
-    # Open up the mesh
-    mesh = np.load(cfg.getpath('mesh', 'file'))
-
     # Get the mapping from physical ranks to MPI ranks
     rallocs = get_rank_allocation(mesh, cfg)
 
-    # Initial conditions
-    if len(sys.argv) == 3:
-        initsoln = np.load(sys.argv[2])
-    else:
-        initsoln = None
-
     # Construct the time integrator
-    integrator = get_integrator(backend, rallocs, mesh, initsoln, cfg)
+    integrator = get_integrator(backend, rallocs, mesh, soln, cfg)
 
     # Execute!
     integrator.run()
