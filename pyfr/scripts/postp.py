ec9bc89372670e623dbe98c34591fba62a0ee64a
Freddie Witherden <freddie@witherden.org>
2013-01-24 15:50:49 +0000

    Rename merge to pack in postp.

--- pyfr/scripts/postp.py
+++ pyfr/scripts/postp.py
@@ -55,16 +55,16 @@ def main():
 
     sp = ap.add_subparsers(help='sub-command help')
 
-    ap_merge = sp.add_parser('pack', help='pack --help', description='Packs a '
-                             'pyfrs-directory into a pyfrs-file.  If no '
-                             'output file is specified then that of the '
-                             'input directory is taken.  This command will '
-                             'replace any existing file or directory.')
-    ap_merge.add_argument('indir', metavar='in',
-                          help='Input PyFR solution directory')
-    ap_merge.add_argument('outf', metavar='out', nargs='?',
-                          help='Out PyFR solution file')
-    ap_merge.set_defaults(process=process_pack)
+    ap_pack = sp.add_parser('pack', help='pack --help', description='Packs a '
+                            'pyfrs-directory into a pyfrs-file.  If no '
+                            'output file is specified then that of the '
+                            'input directory is taken.  This command will '
+                            'replace any existing file or directory.')
+    ap_pack.add_argument('indir', metavar='in',
+                         help='Input PyFR solution directory')
+    ap_pack.add_argument('outf', metavar='out', nargs='?',
+                         help='Out PyFR solution file')
+    ap_pack.set_defaults(process=process_pack)
 
     # Parse the arguments
     args = ap.parse_args()
