8d4350d8ebd162ddc39d2a6768016b754d36b22b
Freddie Witherden <freddie@witherden.org>
2013-03-21 16:58:18 +0000

    Formatting and indentation.

--- pyfr/nputil.py
+++ pyfr/nputil.py
@@ -13,13 +13,14 @@ def npaligned(shape, dtype, alignb=32):
     return buf[off:nbytes + off].view(dtype).reshape(shape)
 
 
-_npeval_syms = {'__builtins__': None,
-                'exp': np.exp, 'log': np.log,
-                'sin': np.sin, 'asin': np.arcsin,
-                'cos': np.cos, 'acos': np.arccos,
-                'tan': np.tan, 'atan': np.arctan, 'atan2': np.arctan2,
-                'abs': np.abs, 'pow': np.power, 'sqrt': np.sqrt,
-                'pi': np.pi}
+_npeval_syms = {
+    '__builtins__': None,
+    'exp': np.exp, 'log': np.log,
+    'sin': np.sin, 'asin': np.arcsin,
+    'cos': np.cos, 'acos': np.arccos,
+    'tan': np.tan, 'atan': np.arctan, 'atan2': np.arctan2,
+    'abs': np.abs, 'pow': np.power, 'sqrt': np.sqrt,
+    'pi': np.pi}
 
 
 def npeval(expr, locals):
@@ -29,14 +30,18 @@ def npeval(expr, locals):
     return eval(expr, _npeval_syms, locals)
 
 
-_range_eval_syms = {'__builtins__': None,
-                    'range': lambda s,e,n: list(np.linspace(s, e, n))}
+_range_eval_syms = {
+    '__builtins__': None,
+    'range': lambda s, e, n: list(np.linspace(s, e, n))}
 
 
 def range_eval(expr):
     return [float(t) for t in eval(expr, _range_eval_syms, None)]
 
-_ctype_map = {np.float32: 'float', np.float64: 'double'}
+
+_ctype_map = {
+    np.int32: 'int', np.uint32: 'unsigned int',
+    np.float32: 'float', np.float64: 'double'}
 
 
 def npdtype_to_ctype(dtype):
