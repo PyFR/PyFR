8d0df121366d88a916cbd95556d8271e930b2dc9
Freddie Witherden <freddie@witherden.org>
2013-06-17 15:04:44 +0100

    Cleanups.

--- pyfr/bases/__init__.py
+++ pyfr/bases/__init__.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
 
 from pyfr.bases.base import BasisBase
-from pyfr.bases.tensorprod import HexBasis, QuadBasis
+from pyfr.bases.tensorprod import (gen_std_hex, gen_std_quad, HexBasis,
+                                   QuadBasis)
