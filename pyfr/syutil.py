5b681f55896af1aec9f71bc86c1f17f60a66e4bd
Freddie Witherden <freddie@witherden.org>
2013-06-26 13:11:26 -0400

    Add a function for generating normalised Jacobi polynomials.

--- pyfr/syutil.py
+++ pyfr/syutil.py
@@ -14,3 +14,12 @@ def lagrange_basis(points, sym):
 
     return [lagrange_poly(n, sym, points, [0]*i + [1] + [0]*(n-i-1)).expand()
             for i in xrange(n)]
+
+
+def norm_jacobi(n, a, b, sym):
+    G, F = sy.gamma, sy.factorial
+
+    N2 = sy.S(2)**(a + b + 1)/(2*n + a + b + 1)\
+       * (G(n + a + 1)*G(n + b + 1))/(F(n)*G(n + a + b + 1))
+
+    return sy.jacobi(n, a, b, sym) / sy.sqrt(N2)
