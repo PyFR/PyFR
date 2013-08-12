1ea110c56c4f1858f3dcdf0bb11893b34e48ad20
Antony Farrington <antony.farrington07@imperial.ac.uk>
2013-08-12 18:23:18 +0100

    Add a time-wise averaging routine to postp.

--- pyfr/scripts/postp.py
+++ pyfr/scripts/postp.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 
 from tempfile import NamedTemporaryFile, mkdtemp
-from argparse import ArgumentParser, FileType, ArgumentTypeError
+from argparse import ArgumentParser, FileType
 
 import numpy as np
 
+from pyfr.inifile import Inifile
+from pyfr.readers.native import read_pyfr_data
 from pyfr.util import rm, all_subclasses
 from pyfr.writers import get_writer_by_name, get_writer_by_extn, BaseWriter
 
@@ -99,6 +102,114 @@ def process_convert(args):
     args.outf.close()
 
 
+def process_tavg(args):
+    # Build list of .pyfrs file names in range
+    if args.rnge:
+        try:
+            # Find common part of .pyfrs file names & min/max of numbering
+            prfx, fmin = args.infs[0].rsplit('.', 1)[0].rsplit('-', 1)
+            prfx2, fmax = args.infs[1].rsplit('.', 1)[0].rsplit('-', 1)
+        except:
+            raise RuntimeError('Range expansion requires file naming of form:'
+                               ' <constant_prefix>-<unique_number>.pyfrs')
+
+        if prfx != prfx2:
+            raise RuntimeError('Range expansion requires a constant prefix '
+                               'in the file name')
+
+        # Parse paths to external directories
+        direc, prfx = os.path.split(prfx)
+        if direc == '':
+            direc = '.'
+
+        # Find .pyfrs files within name range
+        infs = []
+        for filenm in os.listdir(direc):
+            tmp = filenm.rsplit('.', 1)
+
+            # Check for .pyfrs file names with same prefix as in args.infs
+            if tmp[-1] == 'pyfrs' and tmp[0].rsplit('-', 1)[0] == prfx:
+                fprfx, fsufx = tmp[0].rsplit('-', 1)
+
+                # Select files within suffix range defined by args.infs
+                if fsufx >= fmin and fsufx <= fmax:
+                    infs.append(os.path.join(direc, filenm))
+
+        args.infs = sorted(infs)
+
+    if len(args.infs) <= 1:
+        raise RuntimeError('More than one solution file is required for an '
+                           'average')
+
+    # Initialise the rolling average, load the time of first snapshot
+    avg = read_pyfr_data(args.infs[0])
+    tmin = Inifile(avg['stats'].item()).getfloat('time-integration', 'tcurr')
+    cavg = {name: avg[name] for name in list(avg)}
+
+    # Initialise the next file to be averaged, load the time and delta
+    scur = read_pyfr_data(args.infs[1])
+    tcur = Inifile(scur['stats'].item()).getfloat('time-integration', 'tcurr')
+    dtcur = tcur - tmin
+
+    # Verify that solutions were computed on the same mesh
+    if scur['mesh_uuid'] != cavg['mesh_uuid']:
+        raise RuntimeError('Solution files in scope were not computed on '
+                           'the same mesh')
+
+    # Weight the initialised trapezoidal rolling mean
+    for name in avg.soln_files:
+        cavg[name] *= 0.5*dtcur
+
+    # Compute the rolling mean up to the last solution file
+    for i, filenm in enumerate(args.infs[2:]):
+        sys.stdout.write('\rProcessing file: %s' % args.infs[i])
+        sys.stdout.flush()
+
+        # Read in the next solution, obtain the next time and delta
+        snxt = read_pyfr_data(filenm)
+        tnxt = Inifile(snxt['stats'].item()).getfloat('time-integration',
+                                                      'tcurr')
+        dtnxt = tnxt - tcur
+
+        # Verify that solutions were computed on the same mesh
+        if snxt['mesh_uuid'] != cavg['mesh_uuid']:
+            raise RuntimeError('Solution files in scope were not computed on '
+                               'the same mesh')
+
+        # Weight the current solution, then add to the rolling mean
+        weight = 0.5*(dtcur + dtnxt)
+        for name in avg.soln_files:
+            cavg[name] += scur[name]*weight
+
+        # Roll solution pointer, time level and delta
+        scur = snxt
+        tcur = tnxt
+        dtcur = dtnxt
+
+    # Weight final solution, update mean and normalise for elapsed time
+    for name in avg.soln_files:
+        cavg[name] += 0.5*dtcur*scur[name]
+        cavg[name] *= 1.0/(tcur - tmin)
+
+    # Compute and assign stats for a time-averaged solution
+    stats = Inifile()
+    stats.set('time-average', 'tmin', tmin)
+    stats.set('time-average', 'tmax', tcur)
+    stats.set('time-average', 'ntlevels', len(args.infs))
+    cavg['stats'] = stats.tostr()
+
+    # Assign output file name
+    if args.outfile is None:
+        if args.rnge:
+            args.outfile = '-'.join([prfx, 'mean', fmin, 'to',
+                                     fmax]) + '.pyfrs'
+        else:
+            args.outfile = 'time-averaged-solution.pyfrs'
+
+    outf = open(args.outfile, 'wb')
+    np.savez_compressed(outf, **cavg)
+
+
 def main():
     ap = ArgumentParser(prog='pyfr-postp', description='Post processes a '
                         'PyFR simulation')
@@ -173,6 +284,24 @@ def main():
                                'tools.html')
     ap_pap.set_defaults(divisor=None, precision='double',
                         process=process_convert)
+
+    ap_tavg = sp.add_parser('time-avg', help='time-avg --help',
+                            description='Computes the mean solution of a '
+                            'series of PyFR solution (*.pyfrs) files')
+    ap_tavg.add_argument('infs', type=str, nargs='+', help='Names '
+                         'of the (*.pyfrs) solution files to be averaged')
+    ap_tavg.add_argument('-o', '--outfile', type=str, help='Define '
+                         'the name for the (*.pyfrs) output file')
+    ap_tavg.add_argument('-r', '--range', action='store_true', dest='rnge',
+                         help='Usage allows the list of input filenames '
+                         'to be inferred from the contents of a directory.  '
+                         'A "min" then "max" filename is passed for the '
+                         'the [infs ...] arguments. Solution filenames must '
+                         'be consistent and structured of the form:  '
+                         '<constant-prefix>-<unique-numeric>.pyfrs, '
+                         'where the <unique-numeric> is a non-negative '
+                         'integer or real number')
+    ap_tavg.set_defaults(process=process_tavg)
 
     # Parse the arguments
     args = ap.parse_args()
