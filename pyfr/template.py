f0074e3ce0957e4a571ef40a8de15d5d6920de82
Freddie Witherden <freddie@witherden.org>
2013-09-03 22:24:59 +0100

    Minor additions to PkgTemplateLookup.

--- pyfr/template.py
+++ pyfr/template.py
@@ -9,12 +9,23 @@ from mako.template import Template
 
 class PkgTemplateLookup(TemplateLookup):
     def __init__(self, pkg, basedir):
-        self.pkg = pkg
-        self.basedir = basedir
+        self.paths = [(pkg, basedir)]
+
+    def add_search_path(self, pkg, basedir):
+        self.paths.insert(0, (pkg, basedir))
+
+    def del_search_path(self, pkg, basedir):
+        self.paths.remove((pkg, basedir))
 
     def adjust_uri(self, uri, relto):
         return uri
 
     def get_template(self, name):
-        tpl = pkgutil.get_data(self.pkg, os.path.join(self.basedir, name))
-        return Template(tpl, lookup=self)
+        for pkg, basedir in self.paths:
+            try:
+                tpl = pkgutil.get_data(pkg, os.path.join(basedir, name))
+                return Template(tpl, lookup=self)
+            except IOError:
+                pass
+
+        raise RuntimeError('Template "{}" not found'.format(name))
