3fe5f0e196b1a2fa957a78902587fa814c9f1ca8
Freddie Witherden <freddie@witherden.org>
2014-03-07 22:34:11 +0000

    Support additional high-order element types in the Gmsh reader.

--- pyfr/readers/gmsh.py
+++ pyfr/readers/gmsh.py
@@ -35,15 +35,16 @@ class GmshReader(BaseReader):
     name = 'gmsh'
     extn = ['.msh']
 
-    # Gmsh element types to PyFR type (petype) + sizes
+    # Gmsh element types to PyFR type (petype) and node counts
     _etype_map = {
-        1: ('line', 2),  8: ('line', 3), 26: ('line', 4),  27: ('line', 5),
-        2: ('tri', 3),   9: ('tri', 6),  21: ('tri', 10),  23: ('tri', 15),
+        1: ('line', 2), 8: ('line', 3), 26: ('line', 4), 27: ('line', 5),
+        2: ('tri', 3), 9: ('tri', 6), 21: ('tri', 10), 23: ('tri', 15),
         3: ('quad', 4), 10: ('quad', 9), 36: ('quad', 16), 37: ('quad', 25),
-        4: ('tet', 4),  11: ('tet', 10), 29: ('tet', 20),
-        5: ('hex', 8),  12: ('hex', 27), 92: ('hex', 64),
-        6: ('pri', 6),  13: ('pri', 18),
-        7: ('pyr', 5),  14: ('pyr', 14)}
+        4: ('tet', 4), 11: ('tet', 10), 29: ('tet', 20), 30: ('tet', 35),
+        5: ('hex', 8), 12: ('hex', 27), 92: ('hex', 64), 93: ('hex', 125),
+        6: ('pri', 6), 13: ('pri', 18), 90: ('pri', 40), 91: ('pri', 75),
+        7: ('pyr', 5), 14: ('pyr', 14), 118: ('pyr', 30), 119: ('pyr', 55)
+    }
 
     # Number of nodes in the first-order representation an element
     _petype_focount = {v[0]: v[1] for k, v in _etype_map.items() if k < 8}
