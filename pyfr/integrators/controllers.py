e0074fc9c6ec9945e412a13dfcf74f971809be1f
Freddie Witherden <freddie@witherden.org>
2014-10-08 13:57:40 +0100

    Implement step size control for the VdH type RK schemes.
    This is accomplished using a P-type controller.  A PI-type
    controller should follow shortly.

--- pyfr/integrators/controllers.py
+++ pyfr/integrators/controllers.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import math
+
 from pyfr.integrators.base import BaseIntegrator
 from pyfr.util import proxylist
 
@@ -26,6 +28,26 @@ class BaseController(BaseIntegrator):
         # Event handlers for advance_to
         self.completed_step_handlers = proxylist([])
 
+    def _accept_step(self, dt, idxcurr):
+        self.tcurr += dt
+        self.nacptsteps += 1
+        self.nacptchain += 1
+
+        self._idxcurr = idxcurr
+
+        # Filter
+        if self._ffreq and self.nacptsteps % self._ffreq == 0:
+            self._system.filt(idxcurr)
+
+        # Fire off any event handlers
+        self.completed_step_handlers(self)
+
+    def _reject_step(self, idxold):
+        self.nacptchain = 0
+        self.nrjctsteps += 1
+
+        self._idxcurr = idxold
+
     @property
     def nsteps(self):
         return self.nacptsteps + self.nrjctsteps
@@ -37,9 +59,6 @@ class BaseController(BaseIntegrator):
 
 class NoneController(BaseController):
     controller_name = 'none'
-
-    def __init__(self, *args, **kwargs):
-        super(NoneController, self).__init__(*args, **kwargs)
 
     @property
     def _controller_needs_errest(self):
@@ -54,21 +73,88 @@ class NoneController(BaseController):
             dt = max(min(t - self.tcurr, self._dt), self._dtmin)
 
             # Take the step
-            self._idxcurr = self.step(self.tcurr, dt)
+            idxcurr = self.step(self.tcurr, dt)
 
-            # Increment the time
-            self.tcurr += dt
-
-            # Update status
-            self.nacptsteps += 1
-            self.nacptchain += 1
-
-            # Filter
-            if self._ffreq and self.nacptsteps % self._ffreq == 0:
-                self._system.filt(self._idxcurr)
-
-            # Fire off any event handlers
-            self.completed_step_handlers(self)
+            # We are not adaptive, so accept every step
+            self._accept_step(dt, idxcurr)
 
         # Return the solution matrices
+        return self.soln
+
+
+class PController(BaseController):
+    controller_name = 'p'
+
+    def __init__(self, *args, **kwargs):
+        super(PController, self).__init__(*args, **kwargs)
+
+        sect = 'solver-time-integrator'
+
+        # Error tolerances
+        self._atol = self._cfg.getfloat(sect, 'atol')
+        self._rtol = self._cfg.getfloat(sect, 'rtol')
+
+        # Step size adjustment factors
+        self._saffac = self._cfg.getfloat(sect, 'safety-fact', 0.9)
+        self._maxfac = self._cfg.getfloat(sect, 'max-fact', 2.5)
+        self._minfac = self._cfg.getfloat(sect, 'min-fact', 0.3)
+
+    @property
+    def _controller_needs_errest(self):
+        return True
+
+    def _get_errest_kerns(self):
+        try:
+            return self._errest_kerns
+        except AttributeError:
+            self._errest_kerns = self._kernel('errest', nargs=3)
+
+            return self._errest_kerns
+
+    def _errest(self, x, y, z):
+        from mpi4py import MPI
+
+        errest = self._get_errest_kerns()
+
+        # Obtain an estimate for the error
+        self._prepare_reg_banks(x, y, z)
+        self._queue % errest(self._atol, self._rtol)
+
+        # Reduce locally (element types) and globally (MPI ranks)
+        rl = sum(errest.retval)
+        rg = MPI.COMM_WORLD.allreduce(rl, op=MPI.SUM)
+
+        # Normalise
+        err = math.sqrt(rg / self._gndofs)
+
+        return err if not math.isnan(err) else 100
+
+    def advance_to(self, t):
+        if t < self.tcurr:
+            raise ValueError('Advance time is in the past')
+
+        while self.tcurr < t:
+            # Decide on the time step
+            dt = max(min(t - self.tcurr, self._dt), self._dtmin)
+
+            # Take the step
+            idxcurr, idxprev, idxerr = self.step(self.tcurr, dt)
+
+            # Estimate the error
+            err = self._errest(idxerr, idxcurr, idxprev)
+
+            maxf = self._maxfac
+            minf = self._minfac
+            saff = self._saffac
+            sord = self._stepper_order
+
+            # Compute the next time step
+            self._dt = dt*min(maxf, max(minf, saff*err**(-1.0 / sord)))
+
+            # Decide if to accept or reject the step
+            if err < 1.0:
+                self._accept_step(dt, idxcurr)
+            else:
+                self._reject_step(idxprev)
+
         return self.soln
--- pyfr/integrators/steppers.py
+++ pyfr/integrators/steppers.py
@@ -144,16 +144,23 @@ class RK4Stepper(BaseStepper):
 
 
 class RKVdH2RStepper(BaseStepper):
+    # Coefficients
+    a = []
+    b = []
+    bhat = []
+
     def __init__(self, *args, **kwargs):
         super(RKVdH2RStepper, self).__init__(*args, **kwargs)
 
-        # Compute the c coeffs in the Butcher table
+        # Compute the c and error coeffs
         self.c = [0.0] + [sum(self.b[:i]) + ai for i, ai in enumerate(self.a)]
+        self.e = [b - bh for b, bh in zip(self.b, self.bhat)]
+
         self._nstages = len(self.c)
 
     @property
     def _stepper_has_errest(self):
-        return False
+        return self._controller_needs_errest and len(self.bhat)
 
     @property
     def _stepper_nfevals(self):
@@ -161,19 +168,32 @@ class RKVdH2RStepper(BaseStepper):
 
     @property
     def _stepper_nregs(self):
-        return 2
+        return 4 if self._stepper_has_errest else 2
 
     def step(self, t, dt):
         add, rhs = self._add, self._system.rhs
+        errest = self._stepper_has_errest
 
         r1 = self._idxcurr
-        r2, = set(self._regidx) - {r1}
 
+        if errest:
+            r2, rold, rerr = set(self._regidx) - {r1}
+
+            # Save the current solution
+            add(0.0, rold, 1.0, r1)
+        else:
+            r2, = set(self._regidx) - {r1}
+
+        # Evaluate the stages in the scheme
         for i in xrange(self._nstages):
             # Compute -∇·f
             rhs(t + self.c[i]*dt, r2 if i > 0 else r1, r2)
 
-            # Sum; special-casing the final stage
+            # Accumulate the error term in rerr
+            if errest:
+                add(1.0 if i > 0 else 0.0, rerr, self.e[i]*dt, r2)
+
+            # Sum (special-casing the final stage)
             if i < self._nstages - 1:
                 add(1.0, r1, self.a[i]*dt, r2)
                 add((self.b[i] - self.a[i])*dt, r2, 1.0, r1)
@@ -183,7 +203,8 @@ class RKVdH2RStepper(BaseStepper):
             # Swap
             r1, r2 = r2, r1
 
-        return r2
+        # Return
+        return (r2, rold, rerr) if errest else r2
 
 
 class RK34Stepper(RKVdH2RStepper):
@@ -200,6 +221,13 @@ class RK34Stepper(RKVdH2RStepper):
         8237718856693 / 13685301971492,
         57731312506979 / 19404895981398,
         -101169746363290 / 37734290219643
+    ]
+
+    bhat = [
+        15763415370699 / 46270243929542,
+        514528521746 / 5659431552419,
+        27030193851939 / 9429696342944,
+        -69544964788955 / 30262026368149
     ]
 
     @property
@@ -223,6 +251,14 @@ class RK45Stepper(RKVdH2RStepper):
         -1672844663538 / 4480602732383,
         2114624349019 / 3568978502595,
         5198255086312 / 14908931495163
+    ]
+
+    bhat = [
+        1016888040809 / 7410784769900,
+        11231460423587 / 58533540763752,
+        -1563879915014 / 6823010717585,
+        606302364029 / 971179775848,
+        1097981568119 / 3980877426909
     ]
 
     @property
