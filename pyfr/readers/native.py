ba7e5dab29e4c963b20668821fce586471481773
Iyer Arvind Sundaram <a.iyer@imperial.ac.uk>
2015-11-05 09:16:16 +0000

    Minor bug-fix.
    'fname' is needed by the array_info exception.

--- pyfr/readers/native.py
+++ pyfr/readers/native.py
@@ -12,6 +12,7 @@ from pyfr.util import lazyprop, subclasses
 
 class NativeReader(Mapping):
     def __init__(self, fname):
+        self.fname = fname
         self._file = h5py.File(fname, 'r')
 
     def __getitem__(self, aname):
