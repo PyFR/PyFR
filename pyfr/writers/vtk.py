PyFR/PyFR

commit 90942ac3668eb4cf74f62cdf38ff7274ea8a7979

Ensure we do not clobber privarmap and visvarmap in the VTK writer.

pyfr/writers/vtk.py
@@ -25,8 +25,8 @@ class VTKWriter(BaseWriter):
         if self.dataprefix == 'soln':
             self._pre_proc_fields = self._pre_proc_fields_soln
             self._post_proc_fields = self._post_proc_fields_soln
-            self._soln_fields = self.elementscls.privarmap[self.ndims]
-            self._vtk_vars = self.elementscls.visvarmap[self.ndims]
+            self._soln_fields = list(self.elementscls.privarmap[self.ndims])
+            self._vtk_vars = list(self.elementscls.visvarmap[self.ndims])
         # Otherwise we're dealing with simple scalar data
         else:
             self._pre_proc_fields = self._pre_proc_fields_scal

commit 1103945fec230c3be53247f95413177b1ae71e22

Fix a bug introduced in 90942ac.

pyfr/writers/vtk.py
@@ -26,7 +26,7 @@ class VTKWriter(BaseWriter):
             self._pre_proc_fields = self._pre_proc_fields_soln
             self._post_proc_fields = self._post_proc_fields_soln
             self._soln_fields = list(self.elementscls.privarmap[self.ndims])
-            self._vtk_vars = list(self.elementscls.visvarmap[self.ndims])
+            self._vtk_vars = dict(self.elementscls.visvarmap[self.ndims])
         # Otherwise we're dealing with simple scalar data
         else:
             self._pre_proc_fields = self._pre_proc_fields_scal