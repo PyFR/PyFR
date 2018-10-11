e2dfde6ce6babea6983d5ca735cd8a3fd7711117
Freddie Witherden <freddie@witherden.org>
2018-10-11 14:14:30 +0100

    Add support for time-averaging gradients of quantities.
    Gradients can be accessed as grad_<primitive var>_[xyz].

--- pyfr/plugins/tavg.py
+++ pyfr/plugins/tavg.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import re
+
 import numpy as np
 
 from pyfr.inifile import Inifile
@@ -25,6 +27,9 @@ class TavgPlugin(BasePlugin):
                       for k in self.cfg.items(cfgsect)
                       if k.startswith('avg-')]
 
+        # Gradient pre-processing
+        self._init_gradients(intg)
+
         # Save a reference to the physical solution point locations
         self.plocs = intg.system.ele_ploc_upts
 
@@ -47,19 +52,63 @@ class TavgPlugin(BasePlugin):
         self.prevex = self._eval_exprs(intg)
         self.accmex = [np.zeros_like(p) for p in self.prevex]
 
+    def _init_gradients(self, intg):
+        # Determine what gradients, if any, are required
+        self._gradpnames = gradpnames = set()
+        for k, ex in self.exprs:
+            gradpnames.update(re.findall(r'\bgrad_(.+?)_[xyz]\b', ex))
+
+        # If gradients are required then form the relevant operators
+        if gradpnames:
+            self._gradop, self._rcpjact = [], []
+
+            for eles in intg.system.ele_map.values():
+                self._gradop.append(eles.basis.m4)
+
+                # Get the smats at the solution points
+                smat = eles.smat_at_np('upts').transpose(2, 0, 1, 3)
+
+                # Get |J|^-1 at the solution points
+                rcpdjac = eles.rcpdjac_at_np('upts')
+
+                # Product to give J^-T at the solution points
+                self._rcpjact.append(smat*rcpdjac)
+
     def _eval_exprs(self, intg):
         exprs = []
 
+        # Get the primitive variable names
+        pnames = self.elementscls.privarmap[self.ndims]
+
         # Iterate over each element type in the simulation
-        for soln, ploc in zip(intg.soln, self.plocs):
-            # Get the primitive variable names and solutions
-            pnames = self.elementscls.privarmap[self.ndims]
+        for i, (soln, ploc) in enumerate(zip(intg.soln, self.plocs)):
+            # Convert from conservative to primitive variables
             psolns = self.elementscls.con_to_pri(soln.swapaxes(0, 1),
                                                  self.cfg)
 
             # Prepare the substitutions dictionary
-            ploc = dict(zip('xyz', ploc.swapaxes(0, 1)))
-            subs = dict(zip(pnames, psolns), t=intg.tcurr, **ploc)
+            subs = dict(zip(pnames, psolns))
+            subs.update(zip('xyz', ploc.swapaxes(0, 1)))
+
+            # Compute any required gradients
+            if self._gradpnames:
+                # Gradient operator and J^-T matrix
+                gradop, rcpjact = self._gradop[i], self._rcpjact[i]
+                nupts = gradop.shape[1]
+
+                for pname in self._gradpnames:
+                    psoln = subs[pname]
+
+                    # Compute the transformed gradient
+                    tgradpn = np.dot(gradop, psoln)
+                    tgradpn = tgradpn.reshape(self.ndims, nupts, -1)
+
+                    # Untransform this to get the physical gradient
+                    gradpn = np.einsum('ijkl,jkl->ikl', rcpjact, tgradpn)
+                    gradpn = gradpn.reshape(self.ndims, nupts, -1)
+
+                    for dim, grad in zip('xyz', gradpn):
+                        subs['grad_{0}_{1}'.format(pname, dim)] = grad
 
             # Evaluate the expressions
             exprs.append([npeval(v, subs) for k, v in self.exprs])
