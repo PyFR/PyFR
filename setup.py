e659a92eb73c6810f049863a4749b3c2c320fd8a
Freddie Witherden <freddie@witherden.org>
2019-06-21 15:52:16 -0500

    Update setup.py.

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ import sys
 
 
 # Python version
-if sys.version_info[:2] < (3, 3):
-    print('PyFR requires Python 3.3 or newer')
+if sys.version_info[:2] < (3, 5):
+    print('PyFR requires Python 3.5 or newer')
     sys.exit(-1)
 
 # PyFR version
@@ -129,7 +129,9 @@ console_scripts = [
 classifiers = [
     'License :: OSI Approved :: BSD License',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Topic :: Scientific/Engineering'
 ]
 
