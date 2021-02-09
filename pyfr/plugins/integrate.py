Use corrected gradients in plugins (#188).
Use corrected gradients in plugins (#188).

Co-authored-by: SAEZ-MISCHLICH Gonzalo <g.saez-mischlich@daep-saez.isae.fr>

pyfr/plugins/integrate.py
@@ -102,9 +102,13 @@ class IntegratePlugin(BasePlugin):
 
     def _init_gradients(self, intg, rinfo):
         # Determine what gradients, if any, are required
-        self._gradpnames = gradpnames = set()
+        gradpnames = set()
         for ex in self.exprs:
             gradpnames.update(re.findall(r'\bgrad_(.+?)_[xyz]\b', ex))
+
+        privarmap = self.elementscls.privarmap[self.ndims]
+        self._gradpinfo = [(pname, privarmap.index(pname)) 
+                            for pname in gradpnames]
 
         # If gradients are required then form the relevant operators
         if gradpnames:
@@ -129,6 +133,10 @@ class IntegratePlugin(BasePlugin):
         # Get the primitive variable names
         pnames = self.elementscls.privarmap[self.ndims]
 
+        # Compute primitive gradients if required
+        if self._gradpinfo:
+            grads_eles = self._grad_pvars(intg)
+
         # Iterate over each element type in the simulation
         for i, (soln, eleinfo) in enumerate(zip(intg.soln, self.eleinfo)):
             plocs, wts, eset, emask = eleinfo
@@ -143,25 +151,10 @@ class IntegratePlugin(BasePlugin):
             subs = dict(zip(pnames, psolns))
             subs.update(zip('xyz', plocs))
 
-            # Compute any required gradients
-            if self._gradpnames:
-                # Gradient operator and J^-T matrix
-                gradop, rcpjact = self._gradop[i], self._rcpjact[i]
-                nupts = gradop.shape[1]
-
-                for pname in self._gradpnames:
-                    psoln = subs[pname]
-
-                    # Compute the transformed gradient
-                    tgradpn = gradop @ psoln
-                    tgradpn = tgradpn.reshape(self.ndims, nupts, -1)
-
-                    # Untransform this to get the physical gradient
-                    gradpn = np.einsum('ijkl,jkl->ikl', rcpjact, tgradpn)
-                    gradpn = gradpn.reshape(self.ndims, nupts, -1)
-
-                    for dim, grad in zip('xyz', gradpn):
-                        subs[f'grad_{pname}_{dim}'] = grad
+            # Prepare any required gradients
+            for pname, idx in self._gradpinfo:
+                for dim, grad in zip('xyz', grads_eles[i][idx]):
+                    subs[f'grad_{pname}_{dim}'] = grad
 
             for j, v in enumerate(self.exprs):
                 # Evaluate the expression at each point
@@ -171,6 +164,27 @@ class IntegratePlugin(BasePlugin):
                 intvals[j] += np.sum(iex) - np.sum(iex[emask])
 
         return intvals
+
+    def _grad_pvars(self, intg):
+        grads_eles = []
+
+        # Iterate over each element type in the simulation
+        for i, (soln, eleinfo) in enumerate(zip(intg.soln, self.eleinfo)):
+            eset, emask = eleinfo[2:]
+
+            # Subset and transpose the solution
+            soln = soln[..., eset].swapaxes(0, 1)
+
+            # Rearrange and subset gradient data
+            grad_soln = np.rollaxis(intg.grad_soln[i], 2)[..., eset]
+
+            # Transform from conservative to primitive gradients
+            pgrads = self.elementscls.grad_con_to_pri(soln, grad_soln, self.cfg)
+
+            # Store the gradients
+            grads_eles.append(pgrads)
+        
+        return grads_eles
 
     def __call__(self, intg):
         if intg.nacptsteps % self.nsteps == 0:

pyfr/plugins/tavg.py
@@ -69,9 +69,13 @@ class TavgPlugin(PostactionMixin, RegionMixin, BasePlugin):
 
     def _init_gradients(self, intg):
         # Determine what gradients, if any, are required
-        self._gradpnames = gradpnames = set()
+        gradpnames = set()
         for ex in self.aexprs:
             gradpnames.update(re.findall(r'\bgrad_(.+?)_[xyz]\b', ex))
+
+        privarmap = self.elementscls.privarmap[self.ndims]
+        self._gradpinfo = [(pname, privarmap.index(pname)) 
+                            for pname in gradpnames]
 
         # If gradients are required then form the relevant operators
         if gradpnames:
@@ -107,6 +111,10 @@ class TavgPlugin(PostactionMixin, RegionMixin, BasePlugin):
         # Get the primitive variable names
         pnames = self.elementscls.privarmap[self.ndims]
 
+        # Compute primitive gradients if required
+        if self._gradpinfo:
+            grads_eles = self._grad_pvars(intg)
+
         # Iterate over each element type in the simulation
         for i, (j, rgn) in enumerate(self._ele_regions):
             soln = intg.soln[j][..., rgn]
@@ -118,25 +126,10 @@ class TavgPlugin(PostactionMixin, RegionMixin, BasePlugin):
             # Prepare the substitutions dictionary
             subs = dict(zip(pnames, psolns))
 
-            # Compute any required gradients
-            if self._gradpnames:
-                # Gradient operator and J^-T matrix
-                gradop, rcpjact = self._gradop[i], self._rcpjact[i]
-                nupts = gradop.shape[1]
-
-                for pname in self._gradpnames:
-                    psoln = subs[pname]
-
-                    # Compute the transformed gradient
-                    tgradpn = gradop @ psoln
-                    tgradpn = tgradpn.reshape(self.ndims, nupts, -1)
-
-                    # Untransform this to get the physical gradient
-                    gradpn = np.einsum('ijkl,jkl->ikl', rcpjact, tgradpn)
-                    gradpn = gradpn.reshape(self.ndims, nupts, -1)
-
-                    for dim, grad in zip('xyz', gradpn):
-                        subs[f'grad_{pname}_{dim}'] = grad
+            # Prepare any required gradients
+            for pname, idx in self._gradpinfo:
+                for dim, grad in zip('xyz', grads_eles[i][idx]):
+                    subs[f'grad_{pname}_{dim}'] = grad
 
             # Evaluate the expressions
             exprs.append([npeval(v, subs) for v in self.aexprs])
@@ -156,6 +149,25 @@ class TavgPlugin(PostactionMixin, RegionMixin, BasePlugin):
 
         # Stack up the expressions for each element type and return
         return [np.dstack(exs).swapaxes(1, 2) for exs in exprs]
+
+    def _grad_pvars(self, intg):
+        grads_eles = []
+
+        # Iterate over each element type in the simulation
+        for i, (j, rgn) in enumerate(self._ele_regions):
+            # Subset and transpose the solution
+            soln = intg.soln[j][..., rgn].swapaxes(0, 1)
+            
+            # Rearrange and subset gradient data
+            grad_soln = np.rollaxis(intg.grad_soln[j], 2)[..., rgn]
+
+            # Transform from conservative to primitive gradients
+            pgrads = self.elementscls.grad_con_to_pri(soln, grad_soln, self.cfg)
+
+            # Store the gradients
+            grads_eles.append(pgrads)
+
+        return grads_eles
 
     def __call__(self, intg):
         # If we are not supposed to be averaging yet then return