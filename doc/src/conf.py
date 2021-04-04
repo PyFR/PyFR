b7cf66a429c7caccd75c5a85f7b063c4de868a7e
Will Trojak <wt247@tamu.edu>
2021-04-04 10:36:51 -0500

    changed sidebar background colour

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -146,7 +146,7 @@ html_theme_options = {
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
+    'style_nav_header_background': '#94b24c',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
