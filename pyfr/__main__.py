07945c13096973ee824f5045ccc223896516f3c6
Gonzalo Sáez <11050889+GonzaloSaez@users.noreply.github.com>
2021-08-15 09:13:08 +0200

    Fix __main__.py

--- pyfr/__main__.py
+++ pyfr/__main__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
-sys.path.append('/home/gonzalo/PyFR_fork/PyFR/')
+
 from argparse import ArgumentParser, FileType
 import itertools as it
 import os
