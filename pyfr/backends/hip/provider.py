bec8b8e7bf701f2349266ae614f2b8b79915eeca
Freddie Witherden <freddie@witherden.org>
2022-04-02 21:44:32 -0500

    Cleanups.

--- pyfr/backends/hip/provider.py
+++ pyfr/backends/hip/provider.py
@@ -8,8 +8,7 @@ from pyfr.util import memoize
 
 
 def get_grid_for_block(block, nrow, ncol=1):
-    return (int((nrow + (-nrow % block[0])) // block[0]),
-            int((ncol + (-ncol % block[1])) // block[1]), 1)
+    return (-(-nrow // block[0]), -(-ncol // block[1]), 1)
 
 
 class HIPKernelProvider(BaseKernelProvider):
