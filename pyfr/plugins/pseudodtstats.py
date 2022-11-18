4728ecb6ec667fc97ba4e501b54caddef053f85f
Sambit Mishra <sambit98@tamu.edu>
2022-11-17 20:53:01 -0600

    Added UNICODE characters to PseudodtStats Plugin.

--- pyfr/plugins/pseudodtstats.py
+++ pyfr/plugins/pseudodtstats.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import annotations
-from copy import deepcopy
 
 from pyfr.mpiutil import get_comm_rank_root, mpi
 from pyfr.plugins.base import BasePlugin, init_csv
-
-from sigfig import round
-
-import os
 
 import numpy as np
 
@@ -29,13 +24,13 @@ class PseudodtStatsPlugin(BasePlugin):
 
         self.stats = []
 
-        self.tprev = intg.tcurr
+        self.tₚ = intg.tcurr
 
         self.fvars   = intg.system.elementscls.convarmap[self.ndims]
         self.e_types = intg.system.ele_types
 
         # Maximum of 3 Levels of abstraction for the stats of pseudo-dt field
-        self.psdt_stats = {'max': {'all':0}|{p:{'all':0}|{e:{'all':0} for e in self.e_types} for p in self.fvars}, 
+        self.Δτ_stats = {'max': {'all':0}|{p:{'all':0}|{e:{'all':0} for e in self.e_types} for p in self.fvars}, 
                            'min': {'all':0}|{p:{'all':0}|{e:{'all':0} for e in self.e_types} for p in self.fvars}}
 
         self.abstraction = self.cfg.getint(self.cfgsect, 'abstraction', 1)
@@ -43,7 +38,7 @@ class PseudodtStatsPlugin(BasePlugin):
             self.level = self.cfg.getint(self.cfgsect, 'level', intg.cfg.getint('solver','order'))
 
         csv_header =  'n,t,i'
-        for k, v in self.psdt_stats.items():
+        for k, v in self.Δτ_stats.items():
             if k != 'all':
                 csv_header += f',{k}'
                 for vk, vv in v.items():
@@ -59,7 +54,6 @@ class PseudodtStatsPlugin(BasePlugin):
         # The root rank needs to open the output file
         if self.rank == self.root:
             self.outf = init_csv(self.cfg, cfgsect, csv_header)
-            self.n_storage = []
         else:
             self.outf = None
 
@@ -73,22 +67,24 @@ class PseudodtStatsPlugin(BasePlugin):
 
             if iternr == 1:           # We can store the last step's data
                 if 'solver-dual-time-integrator-multip' in intg.cfg.sections():
-                    stats = self.pseudodt_statistics(intg.pseudointegrator.pintgs[self.level].pseudodt_mats)
+                    Δτ_mats = intg.pseudointegrator.pintgs[self.level].pseudodt_mats
                 else:
-                    stats = self.pseudodt_statistics(intg.pseudointegrator.pseudodt_mats)
+                    Δτ_mats = intg.pseudointegrator.pseudodt_mats
+
+                stats = self.Δτ_statistics(Δτ_mats)
 
                 if self.last_appendable != None:
-                    if self.last_tprev != self.tprev:
+                    if self.last_tprev != self.tₚ:
                         self.stats.append((f for f in self.last_appendable))
                         self.prev_npiter = npiter - 1
                 else:
                     self.prev_npiter = 0 
 
-                self.last_tprev = self.tprev
+                self.last_tprev = self.tₚ
 
             self.last_appendable = (npiter, intg.tcurr, npiter-self.prev_npiter , *self.stats_as_list(stats))
 
-        self.tprev = intg.tcurr
+        self.tₚ = intg.tcurr
 
         # If we're the root rank then output
         if self.outf:
@@ -102,7 +98,7 @@ class PseudodtStatsPlugin(BasePlugin):
         # Reset the stats
         self.stats = []
 
-    def pseudodt_statistics(self, pseudodt_mats):
+    def Δτ_statistics(self, Δτ_mats):
         '''
             self.pseudodt_mats is a list of matrices, one for each element type.
             Each matrix is a 3D array of shape (nupts, nvars, neles)
@@ -113,50 +109,50 @@ class PseudodtStatsPlugin(BasePlugin):
 
                 # each element type, each soln point in element, each variable in (p, u, v, w)
                 # Stats obtained over all elements
-                self.psdt_stats['max'][var][e_type]['each'] = pseudodt_mats[i][:, j, :].max(1)
-                self.psdt_stats['min'][var][e_type]['each'] = pseudodt_mats[i][:, j, :].min(1)
+                self.Δτ_stats['max'][var][e_type]['each'] = Δτ_mats[i][:, j, :].max(1)
+                self.Δτ_stats['min'][var][e_type]['each'] = Δτ_mats[i][:, j, :].min(1)
 
                 # each element type, each variable in (p, u, v, w)
                 # Stats obtained over all elements and element soln points
 
-                t_max = np.array(self.psdt_stats['max'][var][e_type]['each'].max())
-                t_min = np.array(self.psdt_stats['min'][var][e_type]['each'].min())
+                tₘₐₓ = np.array(self.Δτ_stats['max'][var][e_type]['each'].max())
+                tₘᵢₙ = np.array(self.Δτ_stats['min'][var][e_type]['each'].min())
 
                 if self.rank != self.root:
-                    self.comm.Reduce(t_max       , None , op=mpi.MAX, root=self.root)
+                    self.comm.Reduce(tₘₐₓ       , None , op=mpi.MAX, root=self.root)
                 else:
-                    self.comm.Reduce(mpi.IN_PLACE, t_max, op=mpi.MAX, root=self.root)
+                    self.comm.Reduce(mpi.IN_PLACE, tₘₐₓ, op=mpi.MAX, root=self.root)
 
                 if self.rank != self.root:
-                    self.comm.Reduce(t_min       , None , op=mpi.MIN, root=self.root)
+                    self.comm.Reduce(tₘᵢₙ       , None , op=mpi.MIN, root=self.root)
                 else:
-                    self.comm.Reduce(mpi.IN_PLACE, t_min, op=mpi.MIN, root=self.root)
+                    self.comm.Reduce(mpi.IN_PLACE, tₘᵢₙ, op=mpi.MIN, root=self.root)
 
-                self.psdt_stats['max'][var][e_type]['all'] = t_max
-                self.psdt_stats['min'][var][e_type]['all'] = t_min
+                self.Δτ_stats['max'][var][e_type]['all'] = tₘₐₓ
+                self.Δτ_stats['min'][var][e_type]['all'] = tₘᵢₙ
 
             # each variable in (p, u, v, w)
             # Stats obtained over all element types, elements and element soln points
-            self.psdt_stats['max'][var]['all'] = max([self.psdt_stats['max'][var][e_type]['all'] for e_type in self.e_types])
-            self.psdt_stats['min'][var]['all'] = min([self.psdt_stats['min'][var][e_type]['all'] for e_type in self.e_types])
+            self.Δτ_stats['max'][var]['all'] = max([self.Δτ_stats['max'][var][e_type]['all'] for e_type in self.e_types])
+            self.Δτ_stats['min'][var]['all'] = min([self.Δτ_stats['min'][var][e_type]['all'] for e_type in self.e_types])
 
         # Stats obtained over all element types, elements, variable in (p, u, v, w) and element soln points
-        self.psdt_stats['max']['all'] = max([self.psdt_stats['max'][var]['all'] for var in self.fvars])
-        self.psdt_stats['min']['all'] = max([self.psdt_stats['min'][var]['all'] for var in self.fvars])
+        self.Δτ_stats['max']['all'] = max([self.Δτ_stats['max'][var]['all'] for var in self.fvars])
+        self.Δτ_stats['min']['all'] = max([self.Δτ_stats['min'][var]['all'] for var in self.fvars])
 
-        return self.psdt_stats
+        return self.Δτ_stats
 
     def stats_as_list(self, stats):
-        psdts = []
+        Δτ_stats_list = []
         for v in stats.values():
-            psdts.append(v['all'])               
+            Δτ_stats_list.append(v['all'])               
             for vk, vv in v.items():
                 if self.abstraction>1:                   
                     if isinstance(vv, dict):    
-                        psdts.append(vv['all'])
+                        Δτ_stats_list.append(vv['all'])
                 if vk != 'all':
                     for vvv in vv.values():
                         if self.abstraction>2:
                             if isinstance(vvv, dict):
-                                psdts.append(vvv['all'])
-        return psdts
+                                Δτ_stats_list.append(vvv['all'])
+        return Δτ_stats_list
