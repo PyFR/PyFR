Fix merging of time-average files without functions or deviations.

pyfr/plugins/tavg.py
@@ -512,8 +512,10 @@ class TavgCLIPlugin(TavgMixin, BaseCLIPlugin):
             elif self.fexprs:
                 fun_avg = self._eval_fun_exprs(avg_acc.swapaxes(0, 1))
                 outstack = (avg_acc, fun_avg, std_acc)
-            else:
+            elif self.std_all:
                 outstack = (avg_acc, std_acc)
+            else:
+                outstack = (avg_acc,)
 
             # Write out the data
             outf[key] = np.hstack(outstack, dtype=self.dtype)