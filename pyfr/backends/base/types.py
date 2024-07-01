f3dc13a7bed4a4fb6127c9721ff5e4fe51b555f6
Peter Vincent <p.vincent@imperial.ac.uk>
2024-07-01 15:15:52 +0100

    Fix pdeps issue across all backends.

--- pyfr/backends/base/types.py
+++ pyfr/backends/base/types.py
@@ -330,6 +330,10 @@ class Graph:
         if kern in self.knodes:
             raise RuntimeError('Can only add a kernel to a graph once')
 
+        # Note our dependencies
+        self.kdeps[kern] = list(deps)
+        self.depk.update(deps)
+
         # Handle priority-enforcing (false) dependencies
         if self.needs_pdeps:
             deps = [*deps, *pdeps]
@@ -339,10 +343,6 @@ class Graph:
 
         # Ask the kernel to add itself
         self.knodes[kern] = kern.add_to_graph(self, rdeps)
-
-        # Note our dependencies
-        self.kdeps[kern] = list(deps)
-        self.depk.update(deps)
 
     def add_all(self, kerns, deps=[], pdeps=[]):
         for k in kerns:
