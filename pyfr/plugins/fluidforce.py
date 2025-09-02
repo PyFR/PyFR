Corrected gradients in fluidforce
Follow up from https://pyfr.discourse.group/t/fluidforce-with-intg-grad-soln/1292/3

pyfr/plugins/fluidforce.py
@@ -192,16 +192,8 @@ class FluidForcePlugin(BaseSolnPlugin):
             fm[0, :ndims] += np.einsum('i...,ij,jik', qwts, p, norms)
 
             if self._viscous:
-                # Get operator and J^-T matrix
-                m4 = self.ff_int.m4[etype]
-                rcpjact = self.ff_int.rcpjact[etype, fidx]
-
-                # Transformed gradient at solution points
-                tduupts = m4 @ uupts.reshape(nupts, -1)
-                tduupts = tduupts.reshape(ndims, nupts, nvars, -1)
-
-                # Physical gradient at solution points
-                duupts = np.einsum('ijkl,jkml->ikml', rcpjact, tduupts)
+                # Corrected gradient at solution points
+                duupts = intg.grad_soln[etype][..., self.ff_int.eidxs[etype, fidx]]
                 duupts = duupts.reshape(ndims, nupts, -1)
 
                 # Interpolate gradient to flux points