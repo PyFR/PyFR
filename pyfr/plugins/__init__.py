282cfeaf0ba5a66dff4b7ec8e311eff377edff01
Philip <hahnpv@protonmail.com>
2026-02-07 09:41:43 -0600

    Adding a stop feature. Issuing a 'touch stop' in the working folder will cause the simulation to stop. The name of the file can be customized by setting file = custom_file in the .ini file

--- pyfr/plugins/__init__.py
+++ pyfr/plugins/__init__.py
@@ -11,6 +11,7 @@ from pyfr.plugins.residual import ResidualPlugin
 from pyfr.plugins.sampler import SamplerCLIPlugin, SamplerPlugin
 from pyfr.plugins.source import SourcePlugin
 from pyfr.plugins.tavg import TavgCLIPlugin, TavgPlugin
+from pyfr.plugins.stop import StopPlugin
 from pyfr.plugins.turbulence import TurbulencePlugin
 from pyfr.plugins.writer import WriterPlugin
 from pyfr.util import subclass_where
