7d20aace682be7236a8c4bb6f365bcd5641982cb
Jiyong Choi <rtrtcom93@yonsei.ac.kr>
2026-02-27 23:24:06 +0900

    Add manual partitioner using regions and split by axis

--- pyfr/partitioners/__init__.py
+++ pyfr/partitioners/__init__.py
@@ -1,10 +1,11 @@
 from pyfr.partitioners.base import BasePartitioner, write_partitioning
 from pyfr.partitioners.kahip import KaHIPPartitioner
+from pyfr.partitioners.manual import ManualPartitioner
 from pyfr.partitioners.metis import METISPartitioner
 from pyfr.partitioners.reconstruct import reconstruct_partitioning
 from pyfr.partitioners.scotch import SCOTCHPartitioner
 from pyfr.util import subclass_where
-from pyfr.partitioners.manual import ManualPartitioner
+
 
 def get_partitioner(name, *args, **kwargs):
     return subclass_where(BasePartitioner, name=name)(*args, **kwargs)
