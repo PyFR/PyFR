748c840a245739f948658b28d40ff7f8e21d0caf
Toby Flynn <toby_flynn@btinternet.com>
2026-05-22 17:33:39 +0100

    Still use valid pdeps for wait events

--- pyfr/backends/opencl/types.py
+++ pyfr/backends/opencl/types.py
@@ -87,7 +87,8 @@ class OpenCLGraph(base.Graph):
             evtidxs[k] = i
 
             # Resolve the event indices of kernels we depend on
-            wait_evts = [evtidxs[dep] for dep in self.kdeps.get(k, [])] or None
+            wait_evts = [evtidxs[dep] for dep in self._alldeps(k)
+                         if dep in evtidxs] or None
 
             klist.append((k, wait_evts, k in self.depk))
 
