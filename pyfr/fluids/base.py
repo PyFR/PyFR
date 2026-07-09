c06483ae3b9cf10d3ab0f15d9f72302aed776f83
Kyle A. Schau <kaschau89@gmail.com>
2026-07-09 08:54:25 -0400

    Re add log/exp

--- pyfr/fluids/base.py
+++ pyfr/fluids/base.py
@@ -17,7 +17,8 @@ class Quantity:
 
 _host_syms = {
     'sqrt': np.sqrt, 'fabs': np.abs, 'pow': np.power,
-    'min': np.minimum, 'max': np.maximum
+    'min': np.minimum, 'max': np.maximum,
+    'log': np.log, 'exp': np.exp
 }
 
 
