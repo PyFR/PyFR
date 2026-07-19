666d6319023131a57265a30020e90cf9a8d9ce11
Kyle A. Schau <kaschau89@gmail.com>
2026-07-19 16:22:43 -0400

    Recompue fd_eps compute into each iteration.

--- pyfr/integrators/implicit/nonlinear/newton.py
+++ pyfr/integrators/implicit/nonlinear/newton.py
@@ -31,9 +31,6 @@ class NewtonSolver(BaseNonlinearSolver):
         # Pick an initial starting guess
         rnorm = initial_guess_fn(u_reg)
 
-        # Choose a suitable finite difference perturbation
-        self._compute_fd_eps(u_reg)
-
         krylov_total = precond_total = 0
 
         for i in range(self._nl_maxiter):
@@ -55,6 +52,9 @@ class NewtonSolver(BaseNonlinearSolver):
             # After we've done at least one step check for converge
             elif rnorm < tol:
                 break
+
+            # Choose a suitable finite difference perturbation
+            self._compute_fd_eps(u_reg)
 
             # Compute the preconditioner
             self._compute_precond(t, u_reg, gamma_dt, self._rhs, f_reg,
